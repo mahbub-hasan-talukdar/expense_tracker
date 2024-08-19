a7bb2956aa17ebda969748f02b79a4b4a7c1feb8
sizer <shahin.reza@brainstation-23.com>
2024-08-19 19:28:13 +0600

    refactor: use a custom decorated button in the expense-details page

--- lib/presentation/item_details/page/expense_details.dart
+++ lib/presentation/item_details/page/expense_details.dart
@@ -10,6 +10,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:rxdart/rxdart.dart';
 import 'package:utilities/extensions/extensions.dart';
 
+import '../widget/button.dart';
+
 class ExpenseDetailsPage extends StatefulWidget {
   static const String path = "expense-details";
 
@@ -93,7 +95,7 @@ class _ExpenseDetailsPageState extends State<ExpenseDetailsPage> {
 
           return ListView.builder(
             controller: _scrollController,
-            physics: ClampingScrollPhysics(),
+            physics: const ClampingScrollPhysics(),
             itemCount: expenses.length + 1,
             itemBuilder: (context, index) {
               if (index < expenses.length) {
@@ -184,7 +186,17 @@ class _ExpenseDetailsPageState extends State<ExpenseDetailsPage> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             (addButtonSnapshot.data!)
-                ? _addButton(context)
+                ? GradientButton(
+                    text: 'Add more...',
+                    onPressed: () {
+                      _isAddButtonVisible.add(false);
+                      _scrollDown();
+                    },
+                    gradientColors: [
+                      Theme.of(context).colorScheme.primary,
+                      Theme.of(context).colorScheme.tertiaryFixed
+                    ],
+                  )
                 : _addNewExpenseForm(),
           ],
         );
@@ -192,19 +204,8 @@ class _ExpenseDetailsPageState extends State<ExpenseDetailsPage> {
     );
   }
 
-  Widget _addButton(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        _isAddButtonVisible.add(false);
-        _scrollDown();
-      },
-      style: _buttonStyle(),
-      child: const Text('Add'),
-    );
-  }
-
   void _scrollDown() {
-    if(_scrollController.hasClients){
+    if (_scrollController.hasClients) {
       _scrollController.animateTo(
         _scrollController.position.maxScrollExtent + 220.0,
         duration: const Duration(milliseconds: 1000),
@@ -237,41 +238,23 @@ class _ExpenseDetailsPageState extends State<ExpenseDetailsPage> {
   }
 
   Widget _saveButton(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(right: 10),
-      child: ElevatedButton(
-        onPressed: () {
-          _isAddButtonVisible.add(true);
-          _bloc.add(AddNewExpense(
-            description: title.text,
-            price: int.parse(price.text),
-            dateTime: widget.dateTime,
-          ));
-          _bloc.add(FetchExpenseEvent(date: widget.dateTime));
-          title.clear();
-          price.clear();
-        },
-        style: _buttonStyle(),
-        child: const Text('Save'),
-      ),
+    return GradientButton(
+      text: 'Save',
+      onPressed: () {
+        _isAddButtonVisible.add(true);
+        _bloc.add(AddNewExpense(
+          description: title.text,
+          price: int.parse(price.text),
+          dateTime: widget.dateTime,
+        ));
+        _bloc.add(FetchExpenseEvent(date: widget.dateTime));
+        title.clear();
+        price.clear();
+      },
+      gradientColors: [
+        Theme.of(context).colorScheme.primary,
+        Theme.of(context).colorScheme.tertiaryFixed
+      ], // Optional: custom gradient colors
     );
   }
-
-  _buttonStyle() {
-    return ElevatedButton.styleFrom(
-      foregroundColor: Colors.white,
-      backgroundColor: Theme.of(context).colorScheme.primary,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(12),
-      ),
-      padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
-      shadowColor: Theme.of(context).colorScheme.primary.withOpacity(0.5),
-      elevation: 10,
-      textStyle: const TextStyle(
-        fontSize: 18,
-        fontWeight: FontWeight.bold,
-      ),
-    );
-  }
-
 }
